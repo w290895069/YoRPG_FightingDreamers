0dff29957e3a7d6424b91eefb2ce46cac8016ae3
akloskowski <akloskowski@stuy.edu>
2017-11-13 13:32:10 -0500

    added v3

---
+++ v3/Monster.java
@@ -0,0 +1,27 @@
+//   ______ _       _     _   _              ______                                        
+//   |  ___(_)     | |   | | (_)             |  _  \                                       
+//   | |_   _  __ _| |__ | |_ _ _ __   __ _  | | | |_ __ ___  __ _ _ __ ___   ___ _ __ ___ 
+//   |  _| | |/ _` | '_ \| __| | '_ \ / _` | | | | | '__/ _ \/ _` | '_ ` _ \ / _ \ '__/ __|
+//   | |   | | (_| | | | | |_| | | | | (_| | | |/ /| | |  __/ (_| | | | | | |  __/ |  \__ \
+//   \_|   |_|\__, |_| |_|\__|_|_| |_|\__, | |___/ |_|  \___|\__,_|_| |_| |_|\___|_|  |___/
+//             __/ |                   __/ |                                               
+//            |___/                   |___/      
+//Adrian Kloskowski
+//APCS1 pd8
+//HW28 - Ye Olde Role Playing Game
+//2017-11-08
+
+public class Monster extends Character {
+	int randRange(int min, int max) { //I honestly don't know why this isn't just a part of Java. I'm so exhausted.
+		int range = (max - min) + 1;     
+		return (int)(Math.random() * range) + min;
+	}
+	
+	public Monster () { //constructor
+		name = "Ye olde monster";
+		HP = 150;
+		atk = randRange(21,65);
+		def = 20;
+		attackRating = 1;
+	}
+}
---
+++ v3/Protagonist.java
@@ -0,0 +1,32 @@
+//   ______ _       _     _   _              ______                                        
+//   |  ___(_)     | |   | | (_)             |  _  \                                       
+//   | |_   _  __ _| |__ | |_ _ _ __   __ _  | | | |_ __ ___  __ _ _ __ ___   ___ _ __ ___ 
+//   |  _| | |/ _` | '_ \| __| | '_ \ / _` | | | | | '__/ _ \/ _` | '_ ` _ \ / _ \ '__/ __|
+//   | |   | | (_| | | | | |_| | | | | (_| | | |/ /| | |  __/ (_| | | | | | |  __/ |  \__ \
+//   \_|   |_|\__, |_| |_|\__|_|_| |_|\__, | |___/ |_|  \___|\__,_|_| |_| |_|\___|_|  |___/
+//             __/ |                   __/ |                                               
+//            |___/                   |___/                                                
+//Adrian Kloskowski
+//APCS1 pd8
+//HW28 - Ye Olde Role Playing Game
+//2017-11-08
+
+public class Protagonist extends Character {
+	
+	public Protagonist (String s) { //constructor
+		name = s; //takes name input
+		HP = 125;
+		atk = 100;
+		def = 40;
+		attackRating = 0.4;
+	}
+    
+	public void specialize() {
+		atk *= 2;
+		def /= 2;
+	}
+	public void normalize() {
+		atk = 100;
+		def = 40;
+	}
+}
